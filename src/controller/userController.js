9021530b6e9b567442bb917e2a44a1a9fa15cb27
sumit kumar <sumokhan76342@gmail.com>
2023-06-26 14:43:29 +0530

    added bcrypt (hash, salt)

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,10 +1,10 @@
 const userModel = require('../models/UserModel')
-
-
+const bcrypt = require('bcryptjs');
 
 const CreateUser = async (req, res) => {
     try {
         let data = req.body
+
 
         //Validation
         if (!data.name) return res.status(400).send({ status: false, message: "Name require" })
@@ -13,8 +13,12 @@ const CreateUser = async (req, res) => {
         let email = await userModel.findOne({ email: data.email })
         if (email) return res.status(404).send({ status: false, message: "Email already exist!" })
 
+        // Creating secure password using bcrypt
+        const salt = await bcrypt.genSalt(10) // Creating salt
+        const secPass = await bcrypt.hash(data.password, salt) //creating hash form of password
+
         //Creating documment
-        const savedData = await userModel.create(data)
+        const savedData = await userModel.create({ name: data.name, email: data.email, password: secPass })
         res.status(201).send({ status: true, message: savedData })
 
     }
