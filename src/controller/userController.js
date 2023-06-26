cbd3e167174e04fb77e507bccedd770293d5b50c
sumit kumar <sumokhan76342@gmail.com>
2023-06-26 12:26:56 +0530

    Added validations

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,15 +1,26 @@
 const userModel = require('../models/UserModel')
 
 
+
 const CreateUser = async (req, res) => {
     try {
         let data = req.body
+
+        //Validation
+        if (!data.name) return res.status(400).send({ status: false, message: "Name require" })
+        if (!data.email) return res.status(400).send({ status: false, message: "email require" })
+        if (!data.password) return res.status(400).send({ status: false, message: "password require" })
+        let email = await userModel.findOne({ email: data.email })
+        if (email) return res.status(404).send({ status: false, message: "Email already exist!" })
+
+        //Creating documment
         const savedData = await userModel.create(data)
-        res.status(201).send(savedData)
+        res.status(201).send({ status: true, message: savedData })
 
     }
     catch (error) {
-        res.send(error)
+        //collecting backend error
+        res.status(500).send({ stauts: false, message: error.message })
 
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose')
 const express = require('express')
-const app = express()
 const routes = require('./Routes/Route.js')
+
+const app = express()
+
 mongoose.connect("mongodb+srv://sumit:sumit@cluster0.8dflsuw.mongodb.net/notebook")
     .then(() => { console.log("MongoDb connected sucessfylly") })
     .catch((err) => { console.log(err) })
 
 app.use(express.json())
+
 app.use('/', routes)
 
-const PORT = 3000 || process.env.PORT
+const PORT = 4000 || process.env.PORT
 
 app.listen(PORT, () => {
     console.log("server running on port", PORT)
